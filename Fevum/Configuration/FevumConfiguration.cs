13afffbf029d219df52cf9f3bd637cf4be764bbb
mfernandez <mfernandez>
2022-03-05 19:07:52 +0100

    Build porperties

---
+++ Fevum/Configuration/FevumConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fevum.Configuration
+{
+    public class FevumConfiguration
+    {
+        public int MyProperty { get; set; }
+    }
+}
